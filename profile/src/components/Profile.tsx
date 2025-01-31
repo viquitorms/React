1cc5f40d63c2603fd030a1585d87db3389f149ed
Victor Magalhães <vicmagalhaes20@gmail.com>
2025-01-31 17:46:42 -0300

    Adding new components profile

---
+++ profile/src/components/Profile.tsx
@@ -0,0 +1,17 @@
+export default function Profile(){
+    return (
+        <main>
+            <div>
+                <img src="" alt="my picture" />
+            </div>
+            <div>
+                <div>
+                    <h1>Victor Magalhães</h1>
+                </div>
+                <div>
+                    <p></p>
+                </div>
+            </div>
+        </main>
+    );
+}
