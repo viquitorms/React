ac2701e26a680ad6f34b42036d85d40e5d9d3a11
Victor Magalhães <vicmagalhaes20@gmail.com>
2025-02-05 14:05:10 -0300

    Completing profile

--- profile/src/components/SkillList.tsx
+++ profile/src/components/SkillList.tsx
@@ -1,12 +1,27 @@
 import Skill from "./Skill";
 import "../styles/SkillList.css"
+import { Skills } from "../data/Skills";
 
 export default function SkillList(){
+
+    function SetEmoji(level: string){
+        switch (level.toUpperCase()){
+            case 'BEGINNER':
+                return "👶🏻"
+            case 'INTERMEDIATE':
+                return "👌🏻"
+            case 'ADVANCED':
+                return "💪🏻"
+            default:
+                return "🚀"
+        }
+    }
+
     return (
         <div className="skillList">
-            <Skill title="React" emoji="⚛️" backgroundColor="Red"/>
-            <Skill title="HTML" emoji="🚀" backgroundColor="Blue"/>
-            <Skill title="JavaScript" emoji="😎" backgroundColor="Black"/>
+            {Skills.map(skill => (
+                <Skill title={skill.skill} emoji={SetEmoji(skill.level)} backgroundColor={skill.color} />
+            ))}
         </div>
     );
 }
---
+++ profile/src/data/Skills.js
@@ -0,0 +1,32 @@
+export const Skills = [
+    {
+        skill: "HTML & CSS",
+        level: "Advanced",
+        color: "#92D4CA"
+    },
+    {
+        skill: "JavaScript",
+        level: "Intermediate",
+        color: "#9392D4"
+    },
+    {
+        skill: "Web Design",
+        level: "Advanced",
+        color: "#D49292"
+    },
+    {
+        skill: "Git & GitHub",
+        level: "Intermediate",
+        color: "#92D49A"
+    },
+    {
+        skill: "React",
+        level: "Advanced",
+        color: "#C4C4C4"
+    },
+    {
+        skill: "Angular",
+        level: "Beginner",
+        color: "#ACD492"
+    },
+]
