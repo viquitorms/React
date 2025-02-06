44ee4d903a59c5c642ad08cc7b122c01a846bde1
Victor Magalhães <vicmagalhaes20@gmail.com>
2025-02-06 14:07:44 -0300

    Adding new project - Steps

---
+++ steps/src/App.tsx
@@ -0,0 +1,12 @@
+import Steps from "./components/Steps"
+import "../index.css"
+
+function App() {
+  return (
+    <>
+      <Steps/>
+    </>
+  )
+}
+
+export default App
---
+++ steps/src/components/Steps.tsx
@@ -0,0 +1,11 @@
+import "../style/Steps.css"
+
+export default function Steps(){
+    return (
+        <div className="steps">
+            <div className="numbers">
+                <span>Oi</span>
+            </div>
+        </div>
+    );
+}
