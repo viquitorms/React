d03565055f86c4300118d618b4668722c64cf77e
Victor Magalhães <vicmagalhaes20@gmail.com>
2025-02-11 14:06:21 -0300

    adding style in steps

--- steps/src/components/Steps.tsx
+++ steps/src/components/Steps.tsx
@@ -4,7 +4,26 @@ export default function Steps(){
     return (
         <div className="steps">
             <div className="numbers">
-                <span>Oi</span>
+                <div className="number">
+                    <span>1</span>
+                </div>
+                <div className="number"> 
+                    <span>1</span>
+                </div>
+                <div className="number">
+                    <span>1</span>
+                </div>
+            </div>
+            <div>
+                <span>Hello!</span>
+            </div>
+            <div className="buttons">
+                <div className="div-button">
+                    <button className="button">Previous</button>
+                </div>
+                <div>
+                    <button className="button">Next</button>
+                </div>
             </div>
         </div>
     );
